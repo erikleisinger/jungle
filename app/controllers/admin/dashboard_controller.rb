4f2906cc292e627747e396a82cf1aae2995a71f0
erikleisinger <erikleisinger@gmail.com>
2020-11-11 21:57:49 +0000

    authentication username/password for admin login now stored as environmental variables.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: 'Jungle', password: 'book'
+
+  http_basic_authenticate_with name: ENV['SECRET_ADMIN_USERNAME'], password: ENV['SECRET_ADMIN_PASS']
 
   def show
   end
-  
+
 end
