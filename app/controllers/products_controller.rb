9a4bf75234f4b500e9477fc4ca8692e64b2fbf5c
erikleisinger <erikleisinger@gmail.com>
2020-11-11 15:48:31 +0000

    Dollar sign issue resolved -- prices now are all formatted as 0##.##

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
+    # uses model to fetch data
     @products = Product.all.order(created_at: :desc)
+    # view
+    # render :index command is implicit
   end
 
   def show
